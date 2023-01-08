a2c3e86c79d76075f7c4b0c9e305c3adc6868ff1
Arshak22 <arshak_kosakyan@edu.aua.am>
2023-01-08 23:52:57 +0400

    Login Form Done

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,18 +1,35 @@
-import React from "react";
+import {React, useState} from "react";
 import Header from "./Components/Header";
 import Home from "./Pages/Home";
 import { Footer } from "./Components/Footer";
 import { Hook } from "./Components/hook";
 import LoginPage from "./Components/LoginPage";
+import { UserContext } from "./Hooks/Context/UserContext";
 
 function App() {
+  const [user, setUser] = useState(
+    {
+        firstName: "-",
+        lastName: "-",
+        position: "No Position",
+        age: 0,
+        gender: "",
+        email: "",
+        phone: "",
+        DOB: "",
+        picture: null
+    }
+  );
   return (
     <div className="App">
+      <UserContext.Provider value={{user, setUser}}> 
       <LoginPage/>
       {/* <Header />
       <Home />
       <Footer /> */}
       {/* <Hook /> */}
+    </UserContext.Provider>
+     
     </div>
   );
 }
--- react-app/src/Components/LoginPage/index.jsx
+++ react-app/src/Components/LoginPage/index.jsx
@@ -1,62 +1,99 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { UserContext } from "../../Hooks/Context/UserContext";
 import "./style.css";
 
 export default function LoginPage() {
+    const {user, setUser} = useContext(UserContext);
+    const [preview, setPreview] = useState();
+
+    const handleLogin = () => {
+        const firstName = document.getElementById('firstName');
+        const lastName = document.getElementById('lastName');
+        const age = document.getElementById('age');
+        const gender = document.querySelector('input[name="gender"]:checked');
+        const position = document.getElementById('position');
+        const email = document.getElementById('email');
+        const phone = document.getElementById('phone');
+        const date = document.getElementById('date');
+        const picture = document.getElementById('picture').files;
+        if(picture.length > 0) {
+            let image = new FileReader();
+            image.onloadend = () => {
+                setPreview(image.result);
+            }
+            image.readAsDataURL(picture[0]);
+        }
+        const newUser = {
+            firstName: firstName.value,
+            lastName: lastName.value,
+            age: age.value,
+            gender: gender.value,
+            position: position.value,
+            email: email.value,
+            phone: phone.value,
+            DOB: date.value,
+            picture: picture.value
+        }
+        setUser(newUser);
+    }
+
     return (
-        <div className="loginForm">
-            <div className="sideMenu">
-                <h1>Dashbord</h1>
-                <h1>Products</h1>
-            </div>
-            <div className="login">
-                
-                <div className="loginHeader">
-                    <div className="userPic">
+            <div className="loginForm">
+                <div className="sideMenu">
+                    <h1>Dashbord</h1>
+                    <h1>Products</h1>
+                </div>
+                <div className="login">
+                    <div className="loginHeader"> 
+                        <div className="userPic">
+                            {preview ? <img src={preview} alt="userAvatar" style={{width: "100%", height: "100%", objectFit: "cover"}} />: null}
+                        </div>
+                        <div className="nameAndPosition">
+                            <h1>{user.firstName} {user.lastName}</h1>
+                            <h1>{user.position}</h1>
+                        </div>
                     </div>
-                    <div className="nameAndPosition">
-                        <h1>--</h1>
-                        <h1>No Position</h1>
+                    <div className="rowLogin">
+                        <div className="colLogin">
+                            <form action="">
+                                <label for="firstName">First Name</label>
+                                <input type="text" id="firstName" name="firstName" placeholder="First Name" required/>
+
+                                <label for="lastName">Last Name</label>
+                                <input type="text" id="lastName" name="lastName" placeholder="Last Name" required/>
+
+                                <label for="age">Age</label>
+                                <input type="number" id="age" name="age" placeholder="Age" required/>
+
+                                <label id="gender" required>
+                                    Gender:
+                                    <div>
+                                        <input type="radio" name="gender" value="male" /> Male
+                                        <input type="radio" name="gender" value="female" /> Female
+                                    </div>
+                                </label>
+                            </form>
+                        </div>
+                        <div className="colLogin">
+                            <label for="postion">Position</label>
+                            <input type="text" id="position" name="position" placeholder="Position" required/>
+
+                            <label for="email">Email</label>
+                            <input type="email" id="email" name="email" placeholder="Email" required/>
+
+                            <label for="phone">Phone Number</label>
+                            <input type="tel" id="phone" name="phone" placeholder="Phone Number" required/>
+
+                            <label for="date">Date of Birth</label>
+                            <input type="date" id="date" aria-describedby="date-format" min="1900-03-01" max="2031-01-01" required/>
+
+                            <label for="picture">Select a file:</label>
+                            <input type="file" id="picture" name="picture" accept="image/*" required></input>
+
+                            <button id="saveLogin" onClick={handleLogin}>Save Changes</button>
+                        </div>
                     </div>
                 </div>
-                <div className="rowLogin">
-                    <div className="colLogin">
-                        <label for="firstName">First Name</label>
-                        <input type="text" id="firstName" name="firstName" placeholder="First Name"/>
-
-                        <label for="lastName">Last Name</label>
-                        <input type="text" id="lastName" name="lastName" placeholder="Last Name"/>
-
-                        <label for="age">Age</label>
-                        <input type="number" id="age" name="age" placeholder="Age"/>
-
-                        <label id="gender">
-                            Gender:
-                            <div>
-                                <input type="radio" name="gender" value="male" /> Male
-                                <input type="radio" name="gender" value="female" /> Female
-                            </div>
-                        </label>
-                    </div>
-                    <div className="colLogin">
-                        <label for="postion">Position</label>
-                        <input type="text" id="position" name="position" placeholder="Position"/>
-
-                        <label for="email">Email</label>
-                        <input type="email" id="email" name="email" placeholder="Email"/>
-
-                        <label for="phone">Phone Number</label>
-                        <input type="tel" id="phone" name="phone" placeholder="Phone Number"/>
-
-                        <label for="date">Date of Birth</label>
-                        <input type="date" id="date" aria-describedby="date-format" min="1900-03-01" max="2031-01-01" />
-
-                        <label for="myfile">Select a file:</label>
-                        <input type="file" id="myfile" name="myfile"></input>
-
-                        <button id="saveLogin">Save Changes</button>
-                    </div>
-                </div>
             </div>
-        </div>
     );
 }
